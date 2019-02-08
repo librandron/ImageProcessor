aa5cec7a91c82bb20e4a1c9f4d781d357b9624e6
Андрей М <librandron@gmail.com>
2019-02-08 13:28:05 +0300

    add menu

--- ImageProcessor/Program.cs
+++ ImageProcessor/Program.cs
@@ -10,6 +10,30 @@ namespace ImageProcessor
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Choose what you want to do:\n Enter '1' and then press 'Enter' to rename photo by date \n Enter '2' and then press 'Enter' to draw date on image");
+            var choose = Console.ReadLine();
+
+            switch (choose)
+            {
+                case "1":
+                     
+                    break;
+
+                case "2":
+                     
+                    break;
+                    
+
+                default:
+                    Console.WriteLine("Wrong command");
+                    break;
+            }
+
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
